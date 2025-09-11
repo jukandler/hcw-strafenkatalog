e6e981f92cb5aa300c13ceb4ef806d4bb49a633e
Julia Kandler <julia.kandler@sz.de>
2025-09-11 09:56:37 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === 'production' ? '/hcw-strafenkatalog/' : '/'
+  // base: process.env.NODE_ENV === 'production' ? '/hcw-strafenkatalog/' : '/'
+  base: '/hcw-strafenkatalog/'
 })
